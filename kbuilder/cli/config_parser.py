bb5334e292830fb2ce7a561bf00369ed48d70654
Dela Anthonio <dell.anthonio@gmail.com>
2016-07-08 14:29:26 -0400

    Add cli/config_parser.py
    This module is responsible for parsing kbuilder.conf files.
    More options will be added later.

---
+++ kbuilder/cli/config_parser.py
@@ -0,0 +1,14 @@
+import os
+from unipath import Path
+from kbuilder.core.kernel import Kernel
+
+"""Parse config files."""
+
+
+def parse_android_kernel_config(app):
+    """Parse an Android kernel config file."""
+    kernel_root = Kernel.find_root(os.getcwd())
+    kernel_config_file = str(Path(kernel_root, '.kbuilder.conf'))
+    kernel_name = str(kernel_root.name)
+    app.config.parse_file(kernel_config_file)
+    app.log.info('Parsed config file for ' + kernel_name)
