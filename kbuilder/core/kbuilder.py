2f43d362824dd1cb58aca2651538084ff9f91a6c
Dela Anthonio <dell.anthonio@gmail.com>
2016-07-11 13:44:48 -0400

    Core: Remove GPL Headers

--- kbuilder/core/kbuilder.py
+++ kbuilder/core/kbuilder.py
@@ -1,18 +1,4 @@
-#!/usr/bin/env python3
-#     Copyright (C) 2016 Dela Anthonio <dell.anthonio@gmail.com>
-#
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-#
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import os
 import shutil
@@ -107,6 +93,7 @@ def build(kernel: Kernel, toolchains: Iterable[Toolchain],
                 kernel.make_ota_package(output_dir=export_dir,
                                         source_dir=ota_package_dir,
                                         extra_version=toolchain.name)
+
 
 def main():
     """Build the kernel with the selected toolchains."""
