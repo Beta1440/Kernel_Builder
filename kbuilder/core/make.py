87cc48738544bd603bca82623ebdf9a463bdf4e4
Dela Anthonio <dell.anthonio@gmail.com>
2016-07-24 16:15:26 -0400

    make: Add function to get last line of output

--- kbuilder/core/make.py
+++ kbuilder/core/make.py
@@ -47,5 +47,24 @@ def make_output(recipe: str, *, jobs: int=os.cpu_count()) -> str:
     return check_output(command, shell=True, universal_newlines=True).rstrip()
 
 
+def make_output_last_line(*args, **kwargs) -> str:
+    """Execute a make recipe in the shell and return output.
+
+    Args:
+        recipe: Recipe to invoke.
+        jobs: Amount of threads to invoke recipe (default os.cpu_count()).
+
+    Raises:
+          A CalledProcessError if the recipe is unsuccessful.
+
+    Returns:
+          Last line of output of make with trailing whitespace trimmed.
+    """
+    return last_line(make_output(*args, **kwargs))
+
+    def last_line(output: str) -> str:
+        return output.split('\n')[-1]
+
+
 def _format_make_command(recipe: str, *, jobs: int) -> str:
     return 'make {} -j{}'.format(recipe, jobs)
