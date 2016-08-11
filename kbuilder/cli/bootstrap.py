1c700e9f9691cb347a77f137ecc8fbd117b56407
Dela Anthonio <dell.anthonio@gmail.com>
2016-08-11 12:28:26 -0400

    bootstrap: Add controllers

--- kbuilder/cli/bootstrap.py
+++ kbuilder/cli/bootstrap.py
@@ -3,8 +3,15 @@
 # All built-in application controllers should be imported, and registered
 # in this file in the same way as KbuilderBaseController.
 
-from cement.core import handler
 from kbuilder.cli.controllers.base import KbuilderBaseController
+from kbuilder.cli.controllers.build_linux import KbuilderLinuxBuildController
+from kbuilder.cli.controllers.build_android import KbuilderAndroidBuildController
+from kbuilder.cli.controllers.gcc import KbuilderGccController
+
 
 def load(app):
-    handler.register(KbuilderBaseController)
+    app.handler.register(KbuilderBaseController)
+    app.handler.register(KbuilderLinuxBuildController)
+    app.handler.register(KbuilderAndroidBuildController)
+    app.handler.register(KbuilderGccController)
+
