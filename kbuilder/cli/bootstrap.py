17d4684addf1369dc9bbed5a88736773d492533a
Dela Anthonio <dell.anthonio@gmail.com>
2017-05-31 14:34:01 -0400

    cli: Rename controllers folder to controller

--- kbuilder/cli/bootstrap.py
+++ kbuilder/cli/bootstrap.py
@@ -3,10 +3,10 @@
 # All built-in application controllers should be imported, and registered
 # in this file in the same way as KbuilderBaseController.
 
-from kbuilder.cli.controllers.base import KbuilderBaseController
-from kbuilder.cli.controllers.linux import KbuilderLinuxBuildController
-from kbuilder.cli.controllers.android import KbuilderAndroidBuildController
-from kbuilder.cli.controllers.gcc import KbuilderGccController
+from kbuilder.cli.controller.base import KbuilderBaseController
+from kbuilder.cli.controller.linux import KbuilderLinuxBuildController
+from kbuilder.cli.controller.android import KbuilderAndroidBuildController
+from kbuilder.cli.controller.gcc import KbuilderGccController
 
 
 def load(app):
